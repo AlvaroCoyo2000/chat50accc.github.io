f440b8cf8b792ffe63555733b80f7e2e43e371a9
AlvaroCoyo2000 <153029851+AlvaroCoyo2000@users.noreply.github.com>
2023-12-11 19:24:38 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyA8RriQJJtfl4MxbKtSwl1uBxmH48clyF0",
-  authDomain: "chatborrado-89c70.firebaseapp.com",
-  projectId: "chatborrado-89c70",
-  storageBucket: "chatborrado-89c70.appspot.com",
-  messagingSenderId: "1002843686378",
-  appId: "1:1002843686378:web:6d2dfed2f6e207c82a5e93",
-  measurementId: "G-2GNRVBFBK2"
+   apiKey: "AIzaSyB78d32Przel09PmVV3QUQoHqrrMKOorbI",
+  authDomain: "chat50accc.firebaseapp.com",
+  projectId: "chat50accc",
+  storageBucket: "chat50accc.appspot.com",
+  messagingSenderId: "121690404445",
+  appId: "1:121690404445:web:c6aaa31b9b6c1cd5fbbfb4"
     });
